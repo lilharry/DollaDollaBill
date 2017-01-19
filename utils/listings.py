d71a464f8ed6eb9abcdcf6a7f3aebbbb58311371
Harry Lum <hlum1@stuy.edu>
2017-01-19 11:53:04 -0500

    add

--- utils/listings.py
+++ utils/listings.py
@@ -1,15 +1,15 @@
 import sqlite3
 import os
 import datetime
+import time
 
-
-def addListing(user, listing, location, type):
+def addListing(user, listing, location, type, details):
     db = sqlite3.connect("data/database.db")
     c = db.cursor() 
     
-    timestamp = now.strftime("%m-%d-%Y %H:%M")
+    timestamp = datetime.datetime.fromtimestamp(time.time()).strftime("%m-%d-%Y %H:%M")
     
-    c.execute("INSERT INTO listings VALUES (?,?,?,?,?)",(listing,user,location,timestamp,type,))
+    c.execute("INSERT INTO listings VALUES (?,?,?,?,?,?)",(listing,user,location,timestamp,type,details))
     
     db.commit()
     db.close()
@@ -23,3 +23,85 @@ def removeListing(id):
     db.commit()
     db.close()
     return 0
+
+def addImageToListing(id,image):
+    db = sqlite3.connect("data/database.db")
+    c = db.cursor() 
+    c.execute("INSERT INTO images VALUES (?,?)",(id,image,))
+    db.commit()
+    db.close()
+    return 0
+
+def addComment(id,user,comment):
+    db = sqlite3.connect("data/database.db")
+    c = db.cursor() 
+    
+    
+    timestamp = datetime.datetime.fromtimestamp(time.time()).strftime("%m-%d-%Y %H:%M")  
+    c.execute("INSERT INTO comments VALUES (?,?,?,?)",(id,user,comment,timestamp,))
+    
+    db.commit()
+    db.close()
+    return 0
+
+def addToWatchlist(user,id):
+    db = sqlite3.connect("data/database.db")
+    c = db.cursor() 
+    c.execute("INSERT INTO watchlist VALUES (?,?)",(user,id,))
+    db.commit()
+    db.close()
+    return 0
+
+def getWatchlist(user):
+    watchlist = []
+    db = sqlite3.connect("data/database.db")
+    c = db.cursor() 
+    c.execute("SELECT id FROM watchlist WHERE user=?",(user,))
+    for x in c:
+        watchlist.append(getListingInfo(x[0]))
+    return watchlist
+
+def getListingInfo(listing):
+    info = []
+    db = sqlite3.connect("data/database.db")
+    c = db.cursor() 
+    c.execute("SELECT * FROM listings WHERE listing=?",(listing,))
+    for x in c:
+        info.append(x)
+    return info
+
+def getListingInfoId(id):
+    info = []
+    db = sqlite3.connect("data/database.db")
+    c = db.cursor() 
+    c.execute("SELECT * FROM listings WHERE rowid=?",(id,))
+    for x in c:
+        info.append(x)
+    return info
+
+def removeWatchlist(user,id):
+    db = sqlite3.connect("data/database.db")
+    c = db.cursor()
+    data = c.execute("DELETE FROM watchlist WHERE user=? AND id=?",(user,id,))
+    db.commit()
+    db.close()
+
+def clearWatchlist(user):
+    db = sqlite3.connect("data/database.db")
+    c = db.cursor()
+    data = c.execute("DELETE FROM watchlist WHERE user=?", (user,))
+    db.commit()
+    db.close()
+
+if __name__ == '__main__':
+    os.chdir('..')
+    addListing('john','shoes','brooklyn','product')
+    print(getListingInfo(1))
+    removeListing(1)
+    print(getListingInfo(1))
+    
+#add user rating to listings
+    
+    
+    
+    
